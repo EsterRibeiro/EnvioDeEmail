303409d98c098e3d92cab6b43aa69cc2a1605353
Ester <ester.ribeiro10101@gmail.com>
2020-11-21 18:21:01 -0300

    Usando mimekit

---
+++ EnvioEmail/Program.cs
@@ -0,0 +1,19 @@
+﻿using EnvioEmail.Services;
+using System;
+using System.Net.Mail;
+
+namespace EnvioEmail
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MailService mailService = new MailService();
+
+            mailService.SendMessage("email", "Primeiro Envio de Email", "Teste");
+           
+        }
+
+       
+    }
+}
---
+++ EnvioEmail/Services/MailService.cs
@@ -0,0 +1,51 @@
+﻿using MailKit.Net.Smtp;
+using MimeKit;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EnvioEmail.Services
+{
+    public class MailService
+    {
+        private readonly string from = "email";
+        private readonly string password = "password";
+
+        public void SendMessage(string to, string subject, string bodyText)
+        {
+            try
+            {
+                MimeMessage message = new MimeMessage();
+
+                message.From.Add(new MailboxAddress(from)); //adiciona quem está enviando
+                message.To.Add(new MailboxAddress(to)); //adiciona o destinatário
+                message.Subject = subject; //Título do e-mail
+
+                message.Body = new TextPart("plain") //textpart?? 
+                {
+                    Text = bodyText
+                };
+
+
+                using (var client = new SmtpClient()) //abrindo o client SMTP
+                {
+                    client.ServerCertificateValidationCallback = (s, c, h, e) => true; //sem validação
+
+                    client.Connect("smtp.gmail.com", 587); //Portas comumente usadas no SMTP => 2525, 25, 587
+                                                           //host outlook => smtp-mail.outlook.com
+
+                    client.Authenticate(from, password);
+
+                    client.Send(message);
+
+                    Console.WriteLine($"E-mail enviado para {to} com sucesso!");
+
+                    client.Disconnect(true);
+                }
+            }
+            catch (Exception ex) { }
+        }
+    }
+        
+    
+}
